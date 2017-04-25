1d8accafe3a5d021623894029dcae46c4d84f37c
Taylor Shuff <tks2n@mtmail.mtsu.edu>
2017-04-25 18:04:31 -0500

    productRepo

--- Bangazon.API/DAL/ProductRepo.cs
+++ Bangazon.API/DAL/ProductRepo.cs
@@ -4,14 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Bangazon.API.Models;
+using System.Data;
+using Dapper;
 
 namespace Bangazon.API.DAL
 {
     public class ProductRepo : IProductRepo
     {
+        readonly IDbConnection _dbConnection;
+
+        public ProductRepo(IDbConnection connection)
+        {
+            _dbConnection = connection;
+        }
         public Product GetProduct(int ProductId)
         {
-            throw new NotImplementedException();
+            var sql = @"SELECT * 
+                    FROM Product 
+                    WHERE ProductId = @ProductId";
+
+            return _dbConnection.Query(sql, Product);
         }
 
         public List<Product> GetProducts()
