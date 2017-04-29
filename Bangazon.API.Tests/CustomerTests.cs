ad24044f3159b05bd70f280a4666497fd5fecfaf
Alex Saber <alexsaber89@gmail.com>
2017-04-29 13:39:16 -0500

    add TestExtensions in order to test return objects; start on GetAllCustomers test

--- Bangazon.API.Tests/CustomerTests.cs
+++ Bangazon.API.Tests/CustomerTests.cs
@@ -7,6 +7,8 @@ using System.Net.Http;
 using System.Web.Http;
 using Bangazon.API.Models;
 using System.Net;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace Bangazon.API.Tests
 {
@@ -15,20 +17,17 @@ namespace Bangazon.API.Tests
     {
         CustomerController _customerController;
         Mock<ICustomerRepo> _mockICustomerRepo;
+        Customer newCustomer;
 
         [TestInitialize]
         public void Initialize()
         {
             _mockICustomerRepo = new Mock<ICustomerRepo>();
             _customerController = new CustomerController(_mockICustomerRepo.Object);
+            
             _customerController.Request = new HttpRequestMessage();
             _customerController.Request.SetConfiguration(new HttpConfiguration());
-        }
-
-        [TestMethod]
-        public void EnsureICanAddACustomer()
-        {
-            var newCustomer = new Customer
+            newCustomer = new Customer
             {
                 Name = "Zoe Ames",
                 StreetAddress = "123 Mockingbird Lane",
@@ -37,11 +36,65 @@ namespace Bangazon.API.Tests
                 Zip = 37209,
                 Phone = "954-427-5454"
             };
+        }
 
+        [TestMethod]
+        public void EnsureICanAddACustomer()
+        {
             var expected_result = HttpStatusCode.OK;
             var actual_result = _customerController.AddCustomer(newCustomer);
 
             Assert.AreEqual(expected_result,actual_result.StatusCode);
+            _mockICustomerRepo.Verify(x => x.AddCustomer(newCustomer), Times.Once);
+        }
+
+        [TestMethod]
+        public void EnsureICanGetCustomerById()
+        {
+            _mockICustomerRepo.Setup(c => c.GetCustomer(It.Is<int>(x => x == 1)))
+                .Returns(new Customer() { Name = "Alex" });
+
+            var actual_result = _customerController.GetCustomer(1);
+
+            Assert.AreEqual("Alex", actual_result.GetContentValue<Customer>().Name);
+        }
+
+        [TestMethod]
+        public void EnsureICanGetAllCustomers()
+        {
+            _mockICustomerRepo.Setup(c => c.GetAllCustomers())
+                .Returns(new List<Customer>() { new Customer()
+                {
+                    Name = "Joe",
+                    StreetAddress = "1445 Deez Lane",
+                    City = "Fort Lauderdale",
+                    State = "FL",
+                    Zip = 33308,
+                    Phone = "954-465-9453"
+                },
+                new Customer()
+                {
+                    Name = "Joe",
+                    StreetAddress = "1445 Deez Lane",
+                    City = "Fort Lauderdale",
+                    State = "FL",
+                    Zip = 33308,
+                    Phone = "954-465-9453"
+                },
+                new Customer()
+                {
+                    Name = "Joe",
+                    StreetAddress = "1445 Deez Lane",
+                    City = "Fort Lauderdale",
+                    State = "FL",
+                    Zip = 33308,
+                    Phone = "954-465-9453"
+                } });
+
+            var actual_result = _customerController.GetAllCustomers();
+
+            Assert.AreEqual("Alex", actual_result.GetContentValue<IEnumerable<Customer>>());
+            _mockICustomerRepo.Verify(x => x.GetAllCustomers(), Times.Once);
         }
     }
 }
